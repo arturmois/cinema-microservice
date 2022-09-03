ff979ce80175fc1661044326388c6b3f2fa7e06b
Artur Moisés <arturmoiscontato@gmail.com>
2022-09-03 11:24:39 -0300

    update

--- movies-service/src/jest.config.js
+++ movies-service/src/jest.config.js
@@ -3,7 +3,8 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: "coverage",
   coveragePathIgnorePatterns: [
-    "\\\\node_modules\\\\"
+    "/node_modules/",
+    "logger.js"
   ],
   coverageProvider: "v8",
   coverageReporters: [
@@ -17,31 +18,37 @@ module.exports = {
   ],
   moduleFileExtensions: [
     "js",
-    "mjs",
-    "cjs",
+    "json",
     "jsx",
     "ts",
     "tsx",
-    "json",
     "node"
   ],
 
+  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
+
+  // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
 
+  // The root directory that Jest should scan for tests and modules within
   rootDir: '.',
 
+  // A list of paths to directories that Jest should use to search for files in
   // roots: [
   //   "<rootDir>"
   // ],
 
+  // The paths to modules that run some code to configure or set up the testing environment before each test
   setupFiles: ["dotenv/config"],
-  testEnvironment: "jest-environment-node",
+
+  testEnvironment: "node",
   testMatch: [
     "**/__tests__/**/*.[jt]s?(x)",
     "**/?(*.)+(spec|test).[tj]s?(x)"
   ],
+
   testPathIgnorePatterns: [
-    "\\\\node_modules\\\\"
+    "/node_modules/"
   ]
 };
