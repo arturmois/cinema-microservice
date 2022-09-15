044f597b1a114c81c5260ae33371a8f40cf159f7
arturmois <arturmoiscontato@gmail.com>
2022-09-15 17:31:07 -0300

    update

--- cinema-catalog-service/src/api/cinemaCatalog.js
+++ cinema-catalog-service/src/api/cinemaCatalog.js
@@ -1,22 +1,42 @@
 module.exports = (app, repository) => {
 
-    app.get('/movies/premieres', async (req, res, next) => {
-        const movies = await repository.getMoviePremieres();
-        if (!movies || !movies.length) return res.sendStatus(404);
+    app.get('/cities', async (req, res, next) => {
+        const cities = await repository.getAllCities();
+
+        res.json(cities);
+    })
+
+    app.get('/cities/:cidyId/movies', async (req, res, next) => {
+        const movies = await repository.getMoviesByCinemaId(req.params.cityId);
+        if (!movies) return res.sendStatus(404);
 
         res.json(movies);
     })
 
-    app.get('/movies/:id', async (req, res, next) => {
-        const movie = await repository.getMovieById(req.params.id);
-        if (!movie) return res.sendStatus(404);
+    app.get('/cities/:cidyId/movies/:movieId', async (req, res, next) => {
+        const sessions = await repository.getMoviesSessionsByCityId(req.params.cityId, req.params.movieId);
+        if (!sessions) return res.sendStatus(404);
 
-        res.json(movie);
+        res.json(sessions);
     })
 
-    app.get('/movies', async (req, res, next) => {
-        const movies = await repository.getAllMovies();
-        if (!movies || !movies.length) return res.sendStatus(404);
+    app.get('/cities/:cidyId/cinemas', async (req, res, next) => {
+        const cinemas = await repository.getCinemasByCityId(req.params.cityId);
+        if (!cinemas) return res.sendStatus(404);
+
+        res.json(cinemas);
+    })
+
+    app.get('/cinemas/:cinemaId/movies', async (req, res, next) => {
+        const movies = await repository.getMoviesByCinemaId(req.params.cinemaId);
+        if (!movies) return res.sendStatus(404);
+
+        res.json(movies);
+    })
+
+    app.get('/cinemas/:cinemaId/movies/movieId', async (req, res, next) => {
+        const movies = await repository.getMoviesSessionsByCinemaId(req.params.cinemaId, req.params.movieId);
+        if (!movies) return res.sendStatus(404);
 
         res.json(movies);
     })
